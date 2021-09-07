5af6d395fb78625943408f53f032adb5143cadf7
Jefferson Cruz <jefferson-cruz@hotmail.com.br>
2021-09-07 10:31:15 -0300

    Fixed connection strings

--- src/services/basket/Basket.API/Entities/ShoppingCart.cs
+++ src/services/basket/Basket.API/Entities/ShoppingCart.cs
@@ -6,7 +6,7 @@ namespace Basket.API.Entities
     public class ShoppingCart
     {
         public string UserName { get; set; }
-        public List<ShoppingCartItem> Items { get; set; }
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();   
         public decimal TotalPrice => GetTotalPrice();
 
         public ShoppingCart()
